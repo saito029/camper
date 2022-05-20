3bf412215cddb9f10dd8d1184d05e42479ee602c
takatoshi saito <takatoshi.saito@farmin.co>
2022-05-20 17:14:36 +0900

    about

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e64cbdc18b2ce1e9d4b7cdd9e2e447daa9e65cbef50cb82e9575acabf39db37ecdcdcf0778e08ebf8e71ffd4f7d7a29a93365a830a364e5b83543c70fcfb9e76'
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top' 
+
+  resources :posts do
+    resources :comments, only:[:create, :destroy]
+    resource :favorites, only: [:create, :destroy] 
+    collection do
+      get 'confirm'
+    end
+  end
+
+  resources :users, only:[:index, :show, :edit, :update] do
+    member do
+      get :follows, :followers
+    end
+    resource :relationships, only: [:create, :destroy]
+  end
+ 
+end
