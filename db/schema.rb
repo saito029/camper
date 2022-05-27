de4936d14e8ca6d6fa62929c6897518082721ea1
takatoshi saito <takatoshi.saito@farmin.co>
2022-05-27 16:49:54 +0900

    いいね機能変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_19_083711) do
+ActiveRecord::Schema.define(version: 2022_05_27_061513) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2022_05_19_083711) do
   end
 
   create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
     t.datetime "created_at", null: false
