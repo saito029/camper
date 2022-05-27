09d898947b763b3f803b21b2b687a998636ef3cb
takatoshi saito <takatoshi.saito@farmin.co>
2022-05-27 15:04:08 +0900

    いいね機能変更前

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
-  attachment :profile_image
-  
-end
-
-class Relationship < ApplicationRecord
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following_user, through: :follower, source: :followed
   has_many :follower_user, through: :followed, source: :follower
+  attachment :profile_image
+  
+
+
 
     # ユーザーをフォローする
   def follow(user_id)
--- db/migrate/20220519031958_create_favorites.rb
+++ db/migrate/20220519031958_create_favorites.rb
@@ -1,10 +1,11 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.integer :user_id
-      t.integer :post_id
+      t.integer :user_id, foreign_key: true
+      t.integer :post_id, foreign_key: true
 
       t.timestamps
+      t.index [:user_id, :item_id], unique: true
     end
   end
 end
